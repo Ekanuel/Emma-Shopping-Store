50feea79c3818e88305514beff02b113b8c99fe3
Ekanuel <ekanumaf@gmail.com>
2023-11-10 12:24:18 +0300

    Emma's E-Commerce store
    This project is simulating an E-Commerce store. The project aims at guiding a visitor from the
    point of selecting the product from the store, signing up with minimal details to activate a profile,
    adding a product into the shopping cart as desired, displaying relevant ads to a visitor given the
    selected product, generating order numbers and keeping track of the order till delivered to the
    customer

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "C001",
+    name: "IPX Camera",
+    price: 45,
+    desc: "Pitch Dark Visual Camera for Night Use.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "S002",
+    name: "Brown Leather Shoes",
+    price: 100,
+    desc: "Elephant Skin Leather Shoes.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "W001",
+    name: "Night Pitch Watch",
+    price: 25,
+    desc: "South African Diamond Watch.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "W002",
+    name: "Sony Watch",
+    price: 300,
+    desc: "Japan Sony Watch 200 Pixels.",
+    img: "images/img-4.jpg",
+  },
+];
